05400fb29f2511e7f5ed8d34c94a4c7be378a061
Eduardo <eduardo.bm10@estudiantec.cr>
2022-05-22 00:47:44 -0600

    Pruebas BNF

--- MayCEy.pl
+++ MayCEy.pl
@@ -20,10 +20,32 @@ pista(p1).
 pista(p21).
 pista(p22).
 pista(p3).
+
+%Comunicacion
+saludo([hola|S],S).
+saludo([[buenos|[dias]]|S],S).
+saludo([[buenas|[tardes]]|S],S).
+
+pregunta_situacion([[cual,es,su,situacion,?]|S],S).
+
 msj_emergencia(mayday).
 msj_emergencia(secuestro7500).
 
+%Gramatica con BNF
+oracion(S0,S):-sintagma_nominal(S0,S1),sintagma_verbal(S1,S).
+oracion(S0,S):-saludo(S0,S1),pregunta_situacion(S1,S).
+
+sintagma_nominal(S0,S):-determinante(S0,S1),sujeto(S1,S).
+
+sintagma_verbal(S0,S):-verbo(S0,S).
+sintagma_verbal(S0,S):-verbo(S0,S1),sintagma_nominal(S1,S).
+
+determinante([el|S],S).
+sujeto([chamo|S],S).
+verbo([come|S],S).
 
 
+escribir(X):-saludo([X|[]],[]),!,fail.
+escribir(X):-write(X).
 
 
