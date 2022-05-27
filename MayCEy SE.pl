8a9e8f87c3872176069ad5b9275c81825cea444d
Eduardo <eduardo.bm10@estudiantec.cr>
2022-05-27 01:05:40 -0600

    Update general

--- MayCEy SE.pl
+++ MayCEy SE.pl
@@ -1,57 +1,89 @@
-/*info_usuario(M):-es_emergencia(M).
-info_usuario(M):-avion(X.
-
-avion(Nombre,Matricula,Velocidad,Aerolinea):-*/
-
 %Base de conocimiento
-es_pequeno(cessna).
-es_pequeno(beechcraft).
-es_pequeno(embraerphenom).
-es_mediano(boing717).
-es_mediano(embraer190).
-es_mediano(airbusA220).
-es_grande(boing747).
-es_grande(airbusA340).
-es_grande(airbusA380).
 
-largo(pista1,1).
-largo(pista2_1,2).
-largo(pista2_2,2).
-largo(pista3,3).
-direccion(este_oeste).
-direccion(oeste_este).
+%Lista de aviones
+aviones_pequenos([cessna, beechcraft,embraerphenom]).
+aviones_medianos([boing717,embraer190,airbusA220]).
+aviones_grandes([boing747,airbusA340,airbusA380]).
 
-pista_1(P):-largo(P,1).
-pista_2_1(P):-largo(P,2),direccion(este_oeste).
-pista_2_2(P):-largo(P,2),direccion(oeste_este).
-pista_3(P):-largo(P,3).
+%Lista de pistas.
+pistas([p1,p2-1,p2-2,p3]).
 
-puede_aterrizar(A,P):-es_pequeno(A),pista_1(P).
-puede_aterrizar(A,P):-es_mediano(A),pista_2_1(P).
-puede_aterrizar(A,P):-es_mediano(A),pista_2_2(P).
-puede_aterrizar(A,P):-es_grande(A),pista_3(P).
-%puede_aterrizar(A,P):-es_pequeno(A),(pista_2_1(P);pista_2_2(P)),!,ocupada(P1),desocupada(P).
+%Lista de palabras clave para determinar una emergencia.
+clave_emergencia([mayday,[perdida,de,motor],parto,[paro,cardiaco],secuestro]).
 
+% Horas en las que las pistas no se encuentran disponibles. Horario 24
+% horas.
+% Orden: ocupada(pista,horas en las que esta ocupada).
+ocupada(p1,[7,10,12,15]).
+ocupada(p2-1,[9,11,13,17]).
+ocupada(p2-2,[7,13,14,16]).
+ocupada(p3,[10,14,16,18]).
 
-%puede_despegar(A,P):-es_pequeno(A),pista_1(P),!,desocupada(P).
-%puede_despegar(A,P,D):-es_mediano(A),D=este_oeste,pista_2_1(P),!,desocupada(P).
-%puede_despegar(A,P,D):-es_mediano(A),D=oeste_este,pista_2_2(P),!,desocupada(P).
-%puede_despegar(A,P):-es_grande(A),pista_3(P),!,desocupada(P).
+%Dirección de las pistas.
+%Orden: direccion(pista,direccion de despegue).
+direccion(p2-1,este_oeste).
+direccion(p2-2,oeste_este).
 
-%Disponibilidad de las pistas.
-%Orden: Hora, lista de pistas disponibles a esa hora.
-%disponibles(7,[pista1,pista2_1,pista2_2,pista3]).
+%puede_aterrizar():-es_emergencia(F).
 
-%disponibles(H,L):-disponibles(H,L).
-%disponibles(H,P):-disponibles(H,[P|_]).
+% Verifica que un avion A puede aterrizar en la pista P, si la pista
+% está designada para ese avion.
+% Orden: puede_aterrizar(avion,pista).
+puede_aterrizar(A,P):-consultar_avion(A,pequeño),P='p1'.
+puede_aterrizar(A,P):-consultar_avion(A,mediano),P='p2-1'.
+puede_aterrizar(A,P):-consultar_avion(A,mediano),P='p2-2'.
+puede_aterrizar(A,P):-consultar_avion(A,grande),P='p3'.
 
-clave_emergencia(mayday).
-clave_emergencia(secuestro).
-%.es_avion(X):-X="ces
+% Verifica que un avion A puede aterrizar en la pista P, si la pista P0
+% que está designada para ese avion está ocupada.
+% Orden: puede_aterrizar(avion,pista,hora de aterrizaje).
+puede_aterrizar(A,P,H):-consultar_avion(A,pequeño),P='p2-1',!,not(esta_ocupada(P,H)),P0='p1',esta_ocupada(P0,H).
+puede_aterrizar(A,P,H):-consultar_avion(A,pequeño),P='p2-2',!,not(esta_ocupada(P,H)),P0='p1',esta_ocupada(P0,H).
+puede_aterrizar(A,P,H):-consultar_avion(A,mediano),P='p3',!,not(esta_ocupada(P,H)),P0='p2-1',esta_ocupada(P0,H).
+puede_aterrizar(A,P,H):-consultar_avion(A,mediano),P='p3',!,not(esta_ocupada(P,H)),P0='p2-2',esta_ocupada(P0,H).
 
-es_emergencia([X|_]):-clave_emergencia(X),!.
-es_emergencia([_|R]):-es_emergencia(R).
+% Verifica que un avion A puede despegar desde la pista P, esto si la
+% pista está designada para ese avion y esta desocupada.
+% Orden: puede_despegar(avion,pista,hora de despegue).
+puede_despegar(A,P,H):-consultar_avion(A,pequeño),consultar_pista(P),!,not(esta_ocupada(P,H)).
+puede_despegar(A,P,H):-consultar_avion(A,grande),consultar_pista(P),!,not(esta_ocupada(P,H)).
 
+% Verifica que un avion A puede despegar desde la pista P, si la pista
+% está designada para ese avion, está desocupada, y además cumple con
+% la direccion a la que se desea despegar.
+%Orden: puede_despegar(avion,pista,hora de despegue,direccion de despegue).
+puede_despegar(A,P,H,Dir):-consultar_avion(A,mediano),direccion(P,Dir),!,not(esta_ocupada(P,H)).
+puede_despegar(A,P,H,Dir):-consultar_avion(A,mediano),direccion(P,Dir),!,not(esta_ocupada(P,H)).
+
+% Revisa si el avion A existe, y además si es un avion pequeño, mediano
+% o grande.
+% Orden: consultar(avion,tamaño).
+consultar_avion(A,T):-aviones_pequenos(L),T='pequeño',miembro(A,L).
+consultar_avion(A,T):-aviones_medianos(L),T='mediano',miembro(A,L).
+consultar_avion(A,T):-aviones_grandes(L),T='grande',miembro(A,L).
+
+%Verifica si una lista está en la lista de pistas.
+consultar_pista(P):-pistas(L),miembro(P,L).
+
+% Verifica si una pista P está ocupada a una hora H.
+% Orden: esta_ocupada(pista,hora).
+esta_ocupada(P,H):-ocupada(P,LH),miembro(H,LH).
+
+% Busca si la frase del usuario contiene una palabra de emergencia y
+% determina si se trata de una.
+detectar_clave(X):-clave_emergencia(L),miembro(X,L).
+es_emergencia(L1):-miembro(X,L1),detectar_clave(X).
+
+% funcion que calcula la hora estimada de aterrizaje de un avion, segun
+% su velocidad, distancia, y hora actual.
+% Orden: hora_de_aterrizaje(velocidad,distancia,hora actual,hora
+% estimada).
+hora_de_aterrizaje(0,0,H,H).
+hora_de_aterrizaje(Vel,Dis,Hora1,HoraX):-hora_de_aterrizaje(0,0,HoraX,HoraX),HoraX is (Vel*Hora1-Dis)/(Vel).
+
+%Funcion miembro de una lista.
+miembro(X,[X|_]).
+miembro(X,[_|T]):-miembro(X,T).
 
 
 
